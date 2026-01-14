8e2259207024eed891f171f527efe7b50cf634ff
SapnaNethmini <sapnanethmini98@gmail.com>
2026-01-14 10:33:01 +0530

    Fix Vercel deploment configurations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,16 +7,16 @@ const config: Config = {
   schema: "./lib/db/schema.ts", 
   out: "./drizzle",                
   dialect: "postgresql",
-//    dbCredentials: {
-//        url: process.env.DATABASE_URL!,
-//     }
- dbCredentials: {
-    host: process.env.DB_HOST || "",       
-    user: process.env.DB_USER || "",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "",
-    url: process.env.DATABASE_URL!,        
-  },
+   dbCredentials: {
+       url: process.env.DATABASE_URL!,
+    },
+//  dbCredentials: {
+//     host: process.env.DB_HOST || "",       
+//     user: process.env.DB_USER || "",
+//     password: process.env.DB_PASSWORD || "",
+//     database: process.env.DB_NAME || "",
+//     url: process.env.DATABASE_URL!,        
+//   },
 };
 
 export default config;
